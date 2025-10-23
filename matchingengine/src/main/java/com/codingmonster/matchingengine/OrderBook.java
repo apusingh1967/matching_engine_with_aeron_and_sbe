f7003d45b75635dca6b1ad3048ced0ceeb3a4c97
apusingh1967 <apusingh1967@gmail.com>
2025-10-22 21:16:22 -0400

    Start work on matching engine

--- matchingengine/src/main/java/com/codingmonster/matchingengine/OrderBook.java
+++ matchingengine/src/main/java/com/codingmonster/matchingengine/OrderBook.java
@@ -11,8 +11,10 @@ public class OrderBook {
   // TODO replace with Agrona off heap structures in future
 
   // price to list of orders in insertion order FIFO from highest to lowest px
+  // use tailMap(key, tru) to read
   private NavigableMap<Long, LinkedList<Order>> buy;
   // price to list of orders in insertion order FIFO from lowest to highest px
+  // use headMap(key, true) to read
   private NavigableMap<Long, LinkedList<Order>> sell;
 
   public OrderBook() {
@@ -24,6 +26,11 @@ public class OrderBook {
     List<Result> results = new ArrayList<>();
 
     switch (order.orderType) {
+      // limit order has price, which could be:
+      // - limit buy above lowest price in sell
+      // - limit sell below highest price in buy
+      // in these cases, try to fill orders immediately which match
+      // and the remainder become a limit order
       case Limit -> {
         if (order.side == Side.Buy) {
           matchLimitBuy(order, results);
@@ -33,6 +40,9 @@ public class OrderBook {
           throw new IllegalArgumentException("Invalid side: " + order.side);
         }
       }
+      // for market orders, ignore price
+      // match immediately, will never rest
+      // unfulfilled quantity will send back exec report as cancelled
       case Market -> {
         if (order.side == Side.Buy) {
           matchMarketBuy(order, results);
@@ -44,11 +54,11 @@ public class OrderBook {
       }
       default -> throw new IllegalArgumentException("Invalid order type: " + order.orderType);
     }
-
     return results;
   }
 
   private void matchLimitBuy(Order order, List<Result> results) {
+    NavigableMap<Long, LinkedList<Order>> orders = this.sell.tailMap(order.price, true);
 
   }
 
